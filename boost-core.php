6d6b4e4c1d5623ae38167c4a621fa0d470d044bf
Matthew <matthewbull@boostonline.co.uk>
2018-01-05 14:23:08 +0000

    Fix the7 theme color box bug

--- boost-core.php
+++ boost-core.php
@@ -14,7 +14,7 @@
  * @wordpress-plugin
  * Plugin Name: 	Boost Core
  * Description: 	Core functionality for WordPress Development.
- * Version: 		0.0.1
+ * Version: 		0.0.2
  * Author: 		Boost
  * Author URI: 		https://boostonlineadvertising.co.uk
  * License: 		GPL-2.0+
