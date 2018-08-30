766e78651b05450a2a278f2ccfad2eae7b8fc28e
Matthew Bull <matthew@matthewbullweb.co.uk>
2018-08-30 15:58:54 +0000

    Write error pages to .htaccess

--- Classes/Activator.php
+++ Classes/Activator.php
@@ -19,6 +19,8 @@ class Activator
 	 */
 	public static function activate() 
 	{
+	    /* Copy custom drop-ins*/
+	    
         $source = __DIR__ . '/../views/maintenance.php';
         $dest = ABSPATH . 'wp-content/maintenance.php';
         copy($source, $dest);
@@ -28,5 +30,33 @@ class Activator
         $source = __DIR__ . '/../views/db-error.php';
         $dest = ABSPATH . 'wp-content/db-error.php';
         copy($source, $dest);
+        
+        /* Create Custom Error Pages in WordPress using HTACCESS
+           Get HTACCESS path & dynamic website url */
+           
+        $path = dirname(__FILE__) . '/../../../../' ;
+        $htaccess_file = $path . '.htaccess';
+        $website_url = get_bloginfo('url').'/';
+        
+        // Check & prevent writing error pages more than once
+        $check_file = file_get_contents($htaccess_file);
+        $this_string = '# BEGIN WordPress Error Pages';
+        
+        if( strpos( $check_file, $this_string ) === false) {
+        
+        // Setup Error page locations dynamically
+        $error_pages .= PHP_EOL. PHP_EOL . '# BEGIN WordPress Error Pages'. PHP_EOL. PHP_EOL;
+        $error_pages .= 'ErrorDocument 401 '.$website_url.'error-401.shtml'.PHP_EOL;
+        $error_pages .= 'ErrorDocument 403 '.$website_url.'error-403.shtml'.PHP_EOL;
+        $error_pages .= 'ErrorDocument 404 '.$website_url.'error-404.shtml'.PHP_EOL;
+        $error_pages .= 'ErrorDocument 500 '.$website_url.'error-500.shtml'.PHP_EOL;
+        $error_pages .= PHP_EOL. '# END WordPress Error Pages'. PHP_EOL;
+        
+        // Write the error page locations to HTACCESS
+        $htaccess = fopen( $htaccess_file, 'a+');
+        fwrite( $htaccess, $error_pages );
+        fclose($htaccess);
+        
+        }
 	}
 }
